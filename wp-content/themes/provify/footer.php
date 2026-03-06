8abae402b830f2c52123f6a179e529def8df61bc
salehbot0 <saleh@cm.sa>
2026-03-07 02:45:53 +0300

    Initial code

---
+++ wp-content/themes/provify/footer.php
@@ -0,0 +1,6 @@
+<footer style="text-align:center;padding:2rem;color:#666;">
+    <p>&copy;  echo date('Y');   bloginfo('name');  - Deployed by Provify</p>
+</footer>
+ wp_footer(); 
+</body>
+</html>
