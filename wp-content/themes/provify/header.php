152a68fd41cc182baaa67f406ac7fab504e5efd2
salehbot0 <saleh@cm.sa>
2026-03-07 02:45:52 +0300

    Initial code

---
+++ wp-content/themes/provify/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html  language_attributes(); >
+<head>
+    <meta charset=" bloginfo('charset'); ">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     wp_head(); 
+</head>
+<body  body_class(); >
+<header class="site-header">
+    <h1> bloginfo('name'); </h1>
+    <p> bloginfo('description'); </p>
+</header>
